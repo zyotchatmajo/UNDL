f988e2a4cb16df0407c0151c7c933bcc6411ac7e
zyotchatmajo <39542528+zyotchatmajo@users.noreply.github.com>
2018-10-13 18:50:17 -0500

    Add files via upload

---
+++ TallerTareas/Ejercicio 4/Ejercicio 4/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] arreglo = new int[,] {
+                { 1,9,23,34,102},
+                { 2,8,67,56,23},
+                { 3,7,84,78,43},
+                { 4,5,90,98,34}
+            };
+            bool xd = false;
+            for (int i = 0; i < 5; i++)
+            {
+                if(xd == true)
+                {
+                    for (int j = 3; j >= 0; j--)
+                    {
+                        Console.Write(arreglo[j,i] + " , ");
+                    }
+                    xd = false;
+                }
+                else
+                {
+                    for (int j = 0; j < 4; j++)
+                    {
+                        Console.Write(arreglo[j, i]+" , ");
+                    }
+                    xd = true;
+                }
+                
+            }
+
+        }
+    }
+}
---
+++ TallerTareas/Tarea 3/Tarea 3/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tarea_3
+{
+    class Program
+    {
+        public struct Numeros
+        {
+            public int x, y;
+
+            public Numeros(int p1, int p2)
+            {
+                x = p1;
+                y = p2;
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Suma de 2 numeros usando Struct");
+            Numeros suma = new Numeros(10, 20);
+            Console.WriteLine("Numero 1: " + suma.x);
+            Console.WriteLine("Numero 2: " + suma.y);
+            Console.WriteLine("La suma de los 2 numeros es : "+(suma.x + suma.y));
+        }
+    }
+}
