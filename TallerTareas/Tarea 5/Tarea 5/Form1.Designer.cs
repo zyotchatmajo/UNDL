e9379ca319ad42d3ded8cbcabaad5fb808d0d89b
zyotchatmajo <39542528+zyotchatmajo@users.noreply.github.com>
2018-10-13 21:17:07 -0500

    Add files via upload

---
+++ TallerTareas/Tarea 5/Tarea 5/Form1.Designer.cs
@@ -0,0 +1,136 @@
+﻿namespace Tarea_5
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.Campo = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button4 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(35, 159);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Agregar";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Campo
+            // 
+            this.Campo.Location = new System.Drawing.Point(123, 97);
+            this.Campo.Name = "Campo";
+            this.Campo.Size = new System.Drawing.Size(100, 20);
+            this.Campo.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(122, 38);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(101, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Introduce el nombre";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(135, 159);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Buscar";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(237, 159);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 4;
+            this.button3.Text = "Eliminar";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(107, 68);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(44, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Nombre";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(84, 215);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(180, 23);
+            this.button4.TabIndex = 6;
+            this.button4.Text = "Mostrar la lista";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(373, 285);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.Campo);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Altas bajas y cambios";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox Campo;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button4;
+    }
+}
+
---
+++ TallerTareas/Tarea 5/Tarea 5/Form1.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Tarea_5
+{
+    public partial class Form1 : Form
+    {
+        List<String> Nombres = new List<String>();
+        int nom = 0;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public void Alta()
+        {
+            Nombres.Add(Campo.Text);
+        }
+
+        public void Cambios()
+        {
+            if(button2.Text == "Buscar")
+            {
+                Console.WriteLine(Nombres.BinarySearch(Campo.Text));
+                nom = Nombres.BinarySearch(Campo.Text);
+                if (nom >= 0)
+                {
+                    label2.Text = "Se encontro el nombre :" + Campo.Text + " Introduce un nuevo nombre";
+                    button2.Text = "Editar";
+                }
+                else
+                {
+                    label2.Text = "No se encontro";
+                }
+            }
+            else
+            {
+                Nombres[nom] = Campo.Text;
+                button2.Text = "Buscar";
+                label2.Text = "Se cambio el nombre ! ";
+            }
+            
+            
+        }
+        public void Baja()
+        {
+            nom = Nombres.BinarySearch(Campo.Text);
+            if (nom >= 0)
+            {
+                label2.Text = "Se encontro el nombre :" + Campo.Text + " y fue eliminado";
+                Nombres.Remove(Campo.Text);
+            }
+            else
+            {
+                label2.Text = "No se encontro";
+            }
+
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Alta();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Cambios();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            var message = string.Join(Environment.NewLine, Nombres);
+            MessageBox.Show(message);
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Baja();
+        }
+    }
+}
